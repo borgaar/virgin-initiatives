f3674fd2816c38ad8ba1820acd891ea2742bb860
Borgar Barland <142639383+borgaar@users.noreply.github.com>
2025-03-20 09:16:58 +0100

    added fade in on hero text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,8 +32,8 @@ export default async function Home() {
 
   return (
     <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center">
-        <div className="flex flex-col items-center gap-12 px-4 py-6">
+      <main className="flex flex-col items-center">
+        <div className="animate-fade-in flex flex-col items-center gap-12 px-4 py-6">
           <h1 className="font-[merriweather] text-5xl leading-[1.4]">
             Where do <span className="italic text-primary">you</span> want to{" "}
             <br />
