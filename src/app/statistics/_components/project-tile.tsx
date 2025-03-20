0e9cafafd166c1961ac834ae1252a64be3dae017
Henrik Utistog Hausberg <henrik.utistog.hausberg@outlook.com>
2025-03-21 00:41:56 +0100

    Improved hover effect on project tile

--- src/app/statistics/_components/project-tile.tsx
+++ src/app/statistics/_components/project-tile.tsx
@@ -40,21 +40,21 @@ export default function ProjectTile({
 }: Project & { idx: number }) {
   return (
     <Link href={`/projects/${idx + 1}`}>
-      <div className="flex flex-col overflow-hidden rounded-lg shadow-lg">
-        <div className="flex-shrink-0">
-          <img
-            className="h-48 w-full object-cover brightness-50 transition-all duration-300 ease-in-out hover:brightness-100"
-            src={banner}
-            alt=""
-          />
-        </div>
-        <div className="flex flex-1 flex-col justify-between bg-neutral-500/5 p-6 backdrop-blur-sm">
+      <div className="group flex flex-col overflow-hidden shadow-lg">
+        <img
+          className="h-48 w-full flex-shrink-0 object-cover brightness-50 saturate-50 transition-all duration-300 ease-in-out group-hover:brightness-75 group-hover:saturate-100"
+          src={banner}
+          alt=""
+        />
+        <div className="flex flex-1 flex-col justify-between bg-neutral-500/5 p-6 backdrop-blur-sm transition duration-300 group-hover:brightness-125">
           <div className="flex-1">
             <p className={`text-sm ${fontColorFromTag(tag)} font-medium`}>
               {tag}
             </p>
-            <p className="text-xl font-semibold text-gray-400">{title}</p>
-            <p className="mt-3 text-base text-gray-500">{description}</p>
+            <p className="mt-2 text-xl font-semibold text-neutral-500">
+              {title}
+            </p>
+            <p className="mt-2 text-base text-neutral-600">{description}</p>
           </div>
           <div className="mt-6 flex items-center">
             <div className="flex-shrink-0">
@@ -66,7 +66,7 @@ export default function ProjectTile({
               />
             </div>
             <div className="ml-3">
-              <p className="text-sm font-medium text-gray-600">
+              <p className="text-sm font-medium text-neutral-600">
                 {participants[0]?.name}
               </p>
             </div>
