b47c783d7bebd84ba18100fd8b6fac8e3ec4e354
Borgar Barland <142639383+borgaar@users.noreply.github.com>
2025-03-20 11:45:04 +0100

    WIP dark mode project page

--- src/app/_components/project/roadmap.tsx
+++ src/app/_components/project/roadmap.tsx
@@ -13,51 +13,55 @@ export interface ProjectRoadmapProps {
 
 export default function ProjectRoadmap({ timeline }: ProjectRoadmapProps) {
   return (
-    <Card className="px-6 py-3">
-      <CardTitle className="mt-4 inline-flex gap-1">Project Roadmap</CardTitle>
-      <CardContent className="mt-4 flow-root pl-0">
-        <ul role="list" className="-mb-8">
-          {timeline.map((event, eventIdx) => (
-            <li key={event.id}>
-              <div className="relative pb-8">
-                {eventIdx !== timeline.length - 1 ? (
-                  <span
-                    aria-hidden="true"
-                    className="absolute left-2 top-2 -ml-px h-full w-0.5 bg-gray-200"
-                  />
-                ) : null}
-                <div className="relative flex space-x-3">
-                  <div>
+    <div className="bg-white bg-opacity-5">
+      <Card className="bg-transparent px-6 py-3">
+        <CardTitle className="mt-4 inline-flex gap-1 text-white">
+          Project Roadmap
+        </CardTitle>
+        <CardContent className="mt-4 flow-root pl-0">
+          <ul role="list" className="-mb-8">
+            {timeline.map((event, eventIdx) => (
+              <li key={event.id}>
+                <div className="relative pb-8">
+                  {eventIdx !== timeline.length - 1 ? (
                     <span
-                      className={cn(
-                        event.status === "completed"
-                          ? "bg-green-600"
-                          : event.status === "in-progress"
-                            ? "bg-yellow-400"
-                            : "bg-gray-200",
-                        "flex size-4 items-center justify-center rounded-full ring-4 ring-card",
-                      )}
-                    ></span>
-                  </div>
-                  <div className="-mt-1 flex min-w-0 flex-1 justify-between space-x-4">
+                      aria-hidden="true"
+                      className="absolute left-2 top-2 -ml-px h-full w-0.5 bg-gray-200"
+                    />
+                  ) : null}
+                  <div className="relative flex space-x-3">
                     <div>
-                      <p className="text-sm text-gray-500">
-                        {event.time.toUpperCase()}
-                      </p>
-                      <h3 className="text-base font-semibold text-foreground">
-                        {event.title}
-                      </h3>
-                      <p className="text-sm text-gray-500">
-                        {event.description}
-                      </p>
+                      <span
+                        className={cn(
+                          event.status === "completed"
+                            ? "bg-green-600"
+                            : event.status === "in-progress"
+                              ? "bg-yellow-400"
+                              : "bg-gray-200",
+                          "flex size-4 items-center justify-center rounded-full ring-4 ring-card",
+                        )}
+                      ></span>
+                    </div>
+                    <div className="-mt-1 flex min-w-0 flex-1 justify-between space-x-4">
+                      <div>
+                        <p className="text-sm text-gray-500">
+                          {event.time.toUpperCase()}
+                        </p>
+                        <h3 className="text-base font-semibold text-foreground">
+                          {event.title}
+                        </h3>
+                        <p className="text-sm text-gray-500">
+                          {event.description}
+                        </p>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </CardContent>
-    </Card>
+              </li>
+            ))}
+          </ul>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
