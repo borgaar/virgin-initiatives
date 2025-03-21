323feb0857d9e44dabafabfa62dcc049e4e7c8fb
Mori <anders.morille@gmail.com>
2025-03-21 03:22:33 +0100

    Super mini patch

--- src/app/_components/project/comments.tsx
+++ src/app/_components/project/comments.tsx
@@ -127,7 +127,7 @@ function AddComment({
       <div className="shrink-0">
         <img
           alt=""
-          src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+          src="https://randomuser.me/api/portraits/thumb/men/40.jpg"
           className="inline-block size-10 rounded-full"
         />
       </div>
